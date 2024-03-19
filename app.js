0074fa95b11a5b80bc9e30017d320453703c5d0d
Saba Kia <skia6589@gmail.com>
2024-03-19 19:15:30 +0000

    fixes

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const getWainwrightsData = async() => {
     const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
     const jsonData = await response.json();
 
-    wainwrightsData = jsonData;
-
-    displayWainwrights(wainwrightsData);
+    return jsonData;
 }
 
-const displayWainwrights = (wainwrights) => {
-    const wainwrightsList = document.getElementById('wainwrights-list');
+let wainwrights;
 
-    wainwrights.forEach(wainwright => {
+async function main(){
+    wainwrights = await getWainwrightsData();
+    console.log(wainwrights);
+    const wainwrightsList = document.getElementById('wainwrights-list');
+    const filteredList = wainwrights.filter(element => {
+        return element["name"].includes("f")
+    });
+    filteredList.forEach(wainwright => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${wainwright.name}, Height: ${wainwright.height}, Area: ${wainwright.area}`;
+        listItem.textContent = `${wainwright["name"]}, Height: ${wainwright["heightMetres"]}, Area: ${wainwright["area"]["areaName"]}`;
         wainwrightsList.appendChild(listItem);
     });
 }
+main();
+
