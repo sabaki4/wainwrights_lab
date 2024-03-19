093afdcc4cbb2012c8415f80eb685dbd12d26c2e
Saba Kia <skia6589@gmail.com>
2024-03-19 17:11:41 +0000

    display, details, form

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const getWainwrightsData = () => {
-    return fetch('https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json')
-        .then(response => response.json());
+const getWainwrightsData = async() => {
+    const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
+    const jsonData = await response.json();
+
+    wainwrightsData = jsonData;
+
+    displayWainwrights(wainwrightsData);
+}
+
+const displayWainwrights = (wainwrights) => {
+    const wainwrightsList = document.getElementById('wainwrights-list');
+
+    wainwrights.forEach(wainwright => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${wainwright.name}, Height: ${wainwright.height}, Area: ${wainwright.area}`;
+        wainwrightsList.appendChild(listItem);
+    });
 }
